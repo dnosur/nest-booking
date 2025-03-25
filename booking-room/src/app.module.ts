3a6405d4a0f0fac14f2692c8a3392c40c36742b9
dnosur <45685967+dnosur@users.noreply.github.com>
2025-03-25 14:27:30 +0200

    init app

--- booking-room/src/app.module.ts
+++ booking-room/src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { SignUpModule } from './sign-up/sign-up.module';
+import { SignInModule } from './sign-in/sign-in.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [UsersModule, SignUpModule],
+  imports: [UsersModule, SignUpModule, SignInModule, RoomsModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
