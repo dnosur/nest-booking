0a41830580230c057132161d8ac44297a20c78b4
dnosur <45685967+dnosur@users.noreply.github.com>
2025-03-25 14:20:24 +0200

    feat(authenticationTokens): add tokens authentication middleware

--- booking-room/dist/middlewares/authenticationTokens.js
+++ booking-room/dist/middlewares/authenticationTokens.js
@@ -12,12 +12,13 @@ const jwt = require("jsonwebtoken");
 const config = require("../config");
 let AuthMiddleware = class AuthMiddleware {
     use(req, res, next) {
-        const token = req.headers['authentication-token'];
-        if (!token) {
+        const authHeader = req.headers['authentication-token'];
+        if (!authHeader) {
             return res
                 .status(401)
                 .json({ message: 'Unauthorized: No token provided' });
         }
+        const token = authHeader.split(' ')[1];
         try {
             const decoded = jwt.verify(token, config.default.jwt.JWT_USER_SECRET);
             req.user = decoded;
